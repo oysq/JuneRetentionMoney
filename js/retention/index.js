7671dca28913829f59e1c1909366fbb926318e2a
oysq <1585228683@qq.com>
2022-06-20 00:12:11 +0800

    首次提交

--- js/retention/index.js
+++ js/retention/index.js
@@ -6,12 +6,13 @@ new Vue({
 			file: {
 				sheetName: null,
 			},
-			sourceData: {
-				jsonData: []
-			}
+			tableData: []
 		}
 	},
+	created() {
+	},
 	methods: {
+		// 读取文件
 		readExcel(event) {
 			// 拿取文件对象
 			const file = event.currentTarget.files[0];
@@ -21,13 +22,83 @@ new Vue({
 			}
 			fileReader.readAsBinaryString(file);
 		},
+		// 读取表格
 		handleExcel(data) {
 			const wb = XLSX.read(data, {
 				type: "binary"
 			});
 			this.file.sheetName = wb.SheetNames[0];
-			this.sourceData.jsonData = XLSX.utils.sheet_to_json(wb.Sheets[this.file.sheetName])
-			console.log(this.sourceData.jsonData)
+			this.tableData = XLSX.utils.sheet_to_json(wb.Sheets[this.file.sheetName], {header: 1, range: 1})
+			this.handleTable();
+		},
+		// 处理表格
+		handleTable() {
+			// console.log(this.tableData)
+			if(isEmpty(this.tableData)) {
+				alert("表格内容为空")
+				return
+			}
+			// this.tableData.forEach(data => this.handleData(data))
+			this.handleData(this.tableData[2])
+		},
+		// 处理一行数据
+		handleData(rowData) {
+			// 是否跳过
+			if(isEmpty(rowData)) {
+				console.log("略过一行空数据")
+				return
+			}
+			console.log("原始数据", rowData)
+
+			// 预处理
+			rowData = this.handleBefore(rowData)
+			console.log("预处理后的数据", rowData)
+
+			// 转换格式
+			const jsonRow = {
+				source: {
+					name: rowData[0],
+					needTotal: rowData[1],
+					needRange: rowData.slice(2, 8),
+					received: rowData[8]
+				},
+				result: {
+					needTotal: 0,
+					needRange: [0, 0, 0, 0, 0, 0]
+				}
+			}
+
+			// 处理优先级
+			this.handlePriority(jsonRow)
+
+		},
+		// 预处理
+		handleBefore(rowData) {
+			// 这里不能用forEach，会跳过空位置
+			let _list = []
+			for (let i = 0; i < rowData.length; i++) {
+				if(isEmpty(rowData[i]) || rowData[i] === '-') {
+					_list.push(0)
+				} else {
+					_list.push(rowData[i])
+				}
+			}
+			return _list
+		},
+		// 处理优先级
+		handlePriority(jsonRow) {
+
+			// 到负数就停（第一个不处理，所以是4开始）
+			for(let i = jsonRow.source.needRange.length-2 ; i >= 0 ; i--) {
+				if(jsonRow.source.needRange[i] < 0) {
+					this.handlePriority_Per(jsonRow.source.needRange[i], jsonRow.source.needRange.slice(i+1, jsonRow.source.needRange.length))
+				}
+			}
+		},
+		// 处理优先级 - 某一行的某一次替换
+		handlePriority_Per(num, list) {
+			console.log('num', num)
+			console.log('list', list)
 		}
 	}
 });
---
+++ js/utils/util.js
@@ -0,0 +1,67 @@
+/**
+ * 判空
+  */
+function isEmpty (val) {
+    // null or undefined
+    if (val == null) return true;
+
+    if (typeof val === 'boolean') return false;
+
+    if (typeof val === 'number') return !val;
+
+    if (val instanceof Error) return val.message === '';
+
+    switch (Object.prototype.toString.call(val)) {
+        // String or Array
+        case '[object String]':
+        case '[object Array]':
+            return !val.length;
+
+        // Map or Set or File
+        case '[object File]':
+        case '[object Map]':
+        case '[object Set]': {
+            return !val.size;
+        }
+        // Plain Object
+        case '[object Object]': {
+            return !Object.keys(val).length;
+        }
+    }
+
+    return false;
+}
+
+/**
+ * 求数组某个长度内的排列
+ * arr: 数组
+ * num：最大个数，num=3 时，表示1、2、3这三种数量级的排列
+ */
+function permutation(arr, num) {
+    let resArr = []; // [[A], [A, B], [A, B, C], ...]
+    let iNow = 0;
+
+    function change(aList, bList, iNow) {
+        for (let i = 0; i < bList.length; i++) {
+
+            // 为了不产生引用关系
+            let _aList = aList.concat();
+            let _bList = bList.concat();
+
+            let _tmp = _bList.splice(i, 1); // 依次去掉 A B C D
+            _aList.push(_tmp[0])
+
+            // 加入结果集
+            resArr.push(_aList)
+
+            if (iNow < num) {
+                change(_aList, _bList, iNow + 1); // 下一层  递归
+            }
+        }
+    }
+    change([], arr, iNow + 1);
+
+    return resArr;
+}
+
+
